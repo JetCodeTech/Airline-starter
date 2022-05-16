14673cdfef186a00bd2ac69541e3b7f60ad76428
praveen-jetcode <105538771+praveen-jetcode@users.noreply.github.com>
2022-05-16 23:24:00 +0530

    flightname

--- src/app/modules/admin/admincomponents/schedule-component/schedule-component.component.ts
+++ src/app/modules/admin/admincomponents/schedule-component/schedule-component.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
   id:number;
+  flightName:string,
   Source: string;
   Destination: string;
   flightDate: string;
@@ -23,7 +24,7 @@ export class ScheduleComponentComponent implements OnInit {
 
 
   ELEMENT_DATA: PeriodicElement[] = [];
-  displayedColumns: string[] = ['Source', 'Destination', 'Flight Date', 'Start Time', 'End Time', 'Number Of Seats', 'Ticket Price', 'Actions'];
+  displayedColumns: string[] = ['No','flightName','Source', 'Destination', 'Flight Date', 'Start Time', 'End Time', 'Number Of Seats', 'Ticket Price', 'Actions'];
   dataSource = this.ELEMENT_DATA;
 
 
