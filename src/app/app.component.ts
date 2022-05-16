928b5425877f1def84e5898f546bf62fd7df3f91
Giridharan-Grids-Guides <59818326+Giridharan-Grids-Guides@users.noreply.github.com>
2022-05-16 22:39:02 +0530

    added toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { toast, TYPE } from './utils/utils'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,12 +1,32 @@
-
-export const setItem =(key: string,value: string)=>{
-  localStorage.setItem(key,value)
+import Swal from 'sweetalert2';
+export const setItem = (key: string, value: string) => {
+  localStorage.setItem(key, value)
 }
 
-export const getItem = (key: string) =>{
-    let val: string = localStorage.getItem(key) || ''
-    if(val.length){
-        return JSON.parse(val)
-    }
-    return null;
+export const getItem = (key: string) => {
+  let val: string = localStorage.getItem(key) || ''
+  if (val.length) {
+    return JSON.parse(val)
+  }
+  return null;
+}
+
+export enum TYPE {
+  ERROR = 'error',
+  SUCCESS = 'success',
+  WARNING = 'warning',
+  INFO = 'info',
+  QUESTION = 'question'
+}
+
+export const toast = (typeIcon = TYPE.SUCCESS, timerProgressBar: boolean = false, title: string) => {
+  Swal.fire({
+    toast: true,
+    position: 'top',
+    showConfirmButton: false,
+    icon: typeIcon,
+    timerProgressBar,
+    timer: 2000,
+    title
+  })
 }
